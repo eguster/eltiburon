676dc4140d0e6af3cc2e2d12fbf230cbe98aea15
eguster <85130036+eguster@users.noreply.github.com>
2021-05-31 13:17:43 -0500

    Add files via upload

---
+++ estilos/indicador de carga.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    const contenedor_loader = document.querySelector('.contenedor')
+    contenedor_loader.style.opacity = 0
+    contenedor_loader.style.visibility = 'hidden'
+})
